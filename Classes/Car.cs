6b3b5efca4c49fc3060d579fdaf5133f20acbf64
juan lopez salamanca <juanlopezsalamanca1@gmail.com>
2023-04-03 10:03:37 -0500

    added notes

---
+++ Classes/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+
+        public void Phrase()
+        {
+            Console.WriteLine($"Your car is a {Make} {Model} and it's from {Year}.");
+        }
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool? Value = null;
+            while (Value == null)
+            {
+                Console.WriteLine("Whtas the make?");
+                var make = Console.ReadLine();
+                Console.WriteLine("Whats the model?");
+                var model = Console.ReadLine();
+                Console.WriteLine("Whats the year?");
+                var year = Convert.ToInt32(Console.ReadLine());
+                Car toyota = new Car(make, model, year);
+                toyota.Phrase();
+                Console.WriteLine("want to register another one?");
+                var answer = Console.ReadLine();
+                switch (answer)
+                {
+                    case "yes":
+                        continue;
+                    default:
+                        Value = true; break;
+                }
+            }
+       
         }
     }
 }
